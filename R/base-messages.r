95528b5fbadf282552ce3cf07817984e9d868716
hadley <h.wickham@gmail.com>
2013-04-09 07:30:59 -0700

    Refactor out common base message

--- R/base-messages.r
+++ R/base-messages.r
@@ -1,22 +1,14 @@
-not_a <- function(call, thing) {
-  paste0("Path '", path, "' does not exist")
+"%is_not%" <- function(call, thing) {
+  paste0(deparse(call[[2]]), " is not ", thing)
 }
 
 # You can provide more useful error message for base functions by adding them
 # to this list
 base_fs <- list(
-  is.character = function(call, env) {
-    paste0(deparse(call[[2]]), " is not a character vector")
-  },
-  is.numeric = function(call, env) {
-    paste0(deparse(call[[2]]), " is not a numeric or integer vector")
-  },
-  is.double = function(call, env) {
-    paste0(deparse(call[[2]]), " is not a numeric vector")
-  },
-  is.integer = function(call, env) {
-    paste0(deparse(call[[2]]), " is not a numeric vector")
-  },
+  is.character = function(call, env) call %is_not% "a character vector",
+  is.numeric = function(call, env) call %is_not% "a numeric or integer vector",
+  is.double = function(call, env) call %is_not% "a numeric vector",
+  is.integer = function(call, env) call %is_not% "a numeric vector",
   file.exists = function(call, env) {
     path <- eval(call[[2]], env)
     paste0("Path '", path, "' does not exist")
