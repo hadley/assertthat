98e7bb0418c2610fd074f74085113df42db7a953
hadley <h.wickham@gmail.com>
2013-04-09 07:50:30 -0700

    More comparisons

--- R/base-logical.r
+++ R/base-logical.r
@@ -1,8 +1,17 @@
 #' @include base.r
 NULL
 
-base_fs$"==" <- function(call, env) {
-  lhs <- paste(deparse(call[[2]]), collapse = "")
-  rhs <- paste(deparse(call[[3]]), collapse = "")
-  paste0(lhs, " does not equal ", rhs)
+logical_is_not <- function(failed) {
+  function(call, env) {
+    lhs <- paste(deparse(call[[2]]), collapse = "")
+    rhs <- paste(deparse(call[[3]]), collapse = "")
+    paste0(lhs, " not ", failed, " ", rhs)
+  }
 }
+
+base_fs$"==" <- logical_is_not("equal to")
+base_fs$"<" <-  logical_is_not("less than")
+base_fs$">" <-  logical_is_not("greater than")
+base_fs$">=" <- logical_is_not("greater than or equal to")
+base_fs$"<=" <- logical_is_not("less than or equal to")
+base_fs$"!=" <- logical_is_not("not equal to")
