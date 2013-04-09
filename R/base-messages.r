7d328df9ff231c24b95ff63cccd3f5ab9bc11dff
hadley <h.wickham@gmail.com>
2013-04-09 07:36:46 -0700

    Even better refactoring of base is messages

--- R/base-messages.r
+++ R/base-messages.r
@@ -1,24 +1,15 @@
-"%is_not%" <- function(call, thing) {
-  paste0(deparse(call[[2]]), " is not ", thing)
-}
-
 base_fs <- new.env(parent = emptyenv())
 
-# You can provide more useful error message for base functions by adding them
-# to this list
+is_not <- function(thing) {
+  function(call, env) {
+    paste0(deparse(call[[2]]), " is not ", thing)
+  }
+}
 
-base_fs$is.character <- function(call, env) {
-  call %is_not% "a character vector"
-}
-base_fs$is.numeric <- function(call, env) {
-  call %is_not% "a numeric or integer vector"
-}
-base_fs$is.double <- function(call, env) {
-  call %is_not% "a numeric vector"
-}
-base_fs$is.integer <- function(call, env) {
-  call %is_not% "a numeric vector"
-}
+base_fs$is.character <- is_not("a character vector")
+base_fs$is.numeric <- is_not("a numeric or integer vector")
+base_fs$is.double <- is_not("a numeric vector")
+base_fs$is.integer <- is_not("a numeric vector")
 
 base_fs$file.exists <- function(call, env) {
   path <- eval(call[[2]], env)
