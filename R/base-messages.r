c675579036dadfede75bb0010b7c64c309e1bb0d
hadley <h.wickham@gmail.com>
2013-04-09 07:33:23 -0700

    Store base messages in an environment

--- R/base-messages.r
+++ R/base-messages.r
@@ -2,20 +2,31 @@
   paste0(deparse(call[[2]]), " is not ", thing)
 }
 
+base_fs <- new.env(parent = emptyenv())
+
 # You can provide more useful error message for base functions by adding them
 # to this list
-base_fs <- list(
-  is.character = function(call, env) call %is_not% "a character vector",
-  is.numeric = function(call, env) call %is_not% "a numeric or integer vector",
-  is.double = function(call, env) call %is_not% "a numeric vector",
-  is.integer = function(call, env) call %is_not% "a numeric vector",
-  file.exists = function(call, env) {
-    path <- eval(call[[2]], env)
-    paste0("Path '", path, "' does not exist")
-  },
-  "==" = function(call, env) {
-    lhs <- paste(deparse(call[[2]]), collapse = "")
-    rhs <- paste(deparse(call[[3]]), collapse = "")
-    paste0(lhs, " does not equal ", rhs)
-  }
-)
+
+base_fs$is.character <- function(call, env) {
+  call %is_not% "a character vector"
+}
+base_fs$is.numeric <- function(call, env) {
+  call %is_not% "a numeric or integer vector"
+}
+base_fs$is.double <- function(call, env) {
+  call %is_not% "a numeric vector"
+}
+base_fs$is.integer <- function(call, env) {
+  call %is_not% "a numeric vector"
+}
+
+base_fs$file.exists <- function(call, env) {
+  path <- eval(call[[2]], env)
+  paste0("Path '", path, "' does not exist")
+}
+
+base_fs$"==" <- function(call, env) {
+  lhs <- paste(deparse(call[[2]]), collapse = "")
+  rhs <- paste(deparse(call[[3]]), collapse = "")
+  paste0(lhs, " does not equal ", rhs)
+}
