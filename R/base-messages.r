64b1f0e0c84d09067b8cf8c13ad7fd09f68c245c
hadley <h.wickham@gmail.com>
2013-04-09 07:44:25 -0700

    More messages for is functions

--- R/base-messages.r
+++ R/base-messages.r
@@ -6,10 +6,39 @@ is_not <- function(thing) {
   }
 }
 
+# Vectors
+base_fs$is.atomic <- is_not("an atomic vector")
 base_fs$is.character <- is_not("a character vector")
-base_fs$is.numeric <- is_not("a numeric or integer vector")
+base_fs$is.complex <- is_not("a complex vector")
 base_fs$is.double <- is_not("a numeric vector")
 base_fs$is.integer <- is_not("a numeric vector")
+base_fs$is.numeric <- is_not("a numeric or integer vector")
+base_fs$is.raw <- is_not("a raw vector")
+base_fs$is.vector <- is_not("an atomic vector without attributes")
+
+# Factors
+base_fs$is.factor <- is_not("a factor")
+base_fs$is.ordered <- is_not("an ordered factor")
+
+# More complicated data structures
+base_fs$is.array <- is_not("an array")
+base_fs$is.data.frame <- is_not("a data frame")
+base_fs$is.list <- is_not("a list")
+base_fs$is.matrix <- is_not("a matrix")
+base_fs$is.null <- is_not("NULL")
+
+# Functions and environments
+base_fs$is.environment <- is_not("an environment")
+base_fs$is.function <- is_not("a function")
+base_fs$is.primitive <- is_not("a primitive function")
+
+# Computing on the language
+base_fs$is.call <- is_not("a quoted call")
+base_fs$is.expression <- is_not("an expression object")
+base_fs$is.name <- is_not("a name")
+base_fs$is.pairlist <- is_not("a pairlist")
+base_fs$is.recursive <- is_not("a recursive object")
+base_fs$is.symbol <- is_not("a name")
 
 base_fs$file.exists <- function(call, env) {
   path <- eval(call[[2]], env)
